bc38e6176e6f46ecd1e6badb8123a9fdd680d0f3
Pavel Tuma <jezevec10@gmail.com>
2017-07-24 15:42:38 +0200

    Removed unused settings, allowed use of 4w

--- tetris_ai/tetris_setting.h
+++ tetris_ai/tetris_setting.h
@@ -1,13 +1,6 @@
-#define PUBLIC_VERSION      0
 #define GAMEMODE_4W         0
-#define PLAYER_WAIT         0
-#define AI_TRAINING_SLOW    1 // ѵ��ģʽ������ʾ
-#define USE4W               0
+#define USE4W               1
 #define ATTACK_MODE         1 // �����У�0���� 1TOP 2��ƴ
 #define AI_SHOW             0 // ���໥������Χ��AI
-#define DISPLAY_NEXT_NUM    6 // ��ʾnext����
 #define AI_TRAINING_DEEP    6 // ѵ��AI˼�����
-#define TRAINING_ROUND      20
-#define AI_TRAINING_0       9
-#define AI_TRAINING_2       6
 #define DEBUG_LEVEL         0
