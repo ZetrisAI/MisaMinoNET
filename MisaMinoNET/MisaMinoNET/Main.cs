819d6c7cb8b6b01d5ba6138d57f22b96b2c22119
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-11-02 22:06:26 +0100

    MisaMinoNET: Read Test

---
+++ MisaMinoNET/MisaMinoNET/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using MisaMinoNET;
+
+namespace MisaMinoNET {
+    public class Interface {
+        [DllImport("libtetris_ai.dll")]
+        public static extern int test();
+    }
+
+    public class MisaMino {
+        public static int test() {
+            return Interface.test();
+        }
+    }
+}
