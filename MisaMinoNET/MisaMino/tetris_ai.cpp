9cb03535d842bbd84ffc8ef21363aeccd9c9a94c
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-09-23 00:34:55 +0200

    Don't scale B2B in TSD only
    Fixes #12.

--- MisaMinoNET/MisaMino/tetris_ai.cpp
+++ MisaMinoNET/MisaMino/tetris_ai.cpp
@@ -440,7 +440,7 @@ namespace AI {
                 int t_att = total_clear_att;
                 double t_clear = total_clears; //+ total_clears / 4.0;
 				double h = pool_h - (double)avg / pool_w;
-                if ( pool.b2b ) s -= (int) ((double)(ai_param.b2b * 5) / (1 + exp(h - 10))); // b2b score
+                if ( pool.b2b ) s -= (int) ((double)(ai_param.b2b * 5) / (1 + (TSD_only? 0 : exp(h - 10)))); // b2b score
                 if ( t_clear > 0 ) {
                     s -= int( ((ai_param.clear_efficient) * ( t_att ) ) );
                 }
